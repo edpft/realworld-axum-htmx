ad4832844e181cdaf371cf5fdc49b1e0b22e3fcd
Ed Fawcett-Taylor <65284206+edpft@users.noreply.github.com>
2026-04-18 17:43:08 +0100

    Update flake.nix with new content

--- flake.nix
+++ flake.nix
@@ -1,1 +1,35 @@
-{ ".\": { "inputs": [ "rust-bin", "mold", "clang", "bacon", "cargo-nextest", "pkg-config", "postgresql" ], "shellHook": "export RUSTFLAGS='-C linker=mold -C target-cpu=native'" }}
+{
+  description = "RealWorld demo app in Rust using Axum and HTMX";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+            pkgs.mold
+            pkgs.clang
+            pkgs.bacon
+            pkgs.cargo-nextest
+            pkgs.pkg-config
+            pkgs.postgresql
+          ];
+
+          shellHook = ''
+            export RUSTFLAGS="-C linker=mold -C target-cpu=native"
+          '';
+        };
+      }
+    );
+}
